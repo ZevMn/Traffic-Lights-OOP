df7a98b0cdf7f37d362c9bbe5e0fe1217340021b
Zev Menachemson <zm1224@imperial.ac.uk>
2024-10-31 09:59:19 +0000

    Started working on building the trafflicLight class

--- trafficLight.cpp
+++ trafficLight.cpp
@@ -1,2 +1,33 @@
-/* Implementation file for trafficLight.h */
+/* trafficLight.cpp - Implementation file for the class trafficLight */
+
+#include <iostream>
+#include "trafficLight.h"
+
+using namespace std;
+
+/*********************** Class TrafficLight ***************************/
+
+class TrafficLight {
+
+	public:
+
+		TrafficLight(Time,char*);
+		TrafficLight(Time,char*,TrafficLight&);
+
+		void carWantsToCross();
+
+		static void setTheTime(Time&);
+
+		friend std::ostream& operator << (std::ostream&, TrafficLight*);
+
+	private:
+
+		/* add members and operations to complete the class yourself */
+		// Delay const
+		// Name const
+		// Pairing const
+		// Colour
+
+};
+
 
