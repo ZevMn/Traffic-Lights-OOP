aa1f8693f3ac23d10a5e90965a8e87663f1ef201
Zev Menachemson <zm1224@imperial.ac.uk>
2024-11-18 11:59:15 +0000

    Started adding docstrings

--- time.h
+++ time.h
@@ -1,7 +1,7 @@
 /* time.h - header file for the class Time */
 
 /* Author: Zev Menachemson 
- * Last Edited: 05/11/2024
+ * Last Edited: 18/11/2024
  */
 
 #ifndef TIME_H
@@ -16,22 +16,51 @@ class Time {
 	public:
 
 		/* Constructors */
+
+		/*
+		 * Default constructor. Initialises time to 00:00:00.
+		 */
 		Time();
+
+		/* 
+		 * Parameterised constructor. Initialises time to given values
+		 * for hours, minutes and seconds.
+		 *
+		 * @param hours Integer value for hours (0-23).
+		 * @param mins Integer value for minutes (0-59).
+		 * @param secs Integer value for seconds (0-59).
+		 */
 		Time(int hours, int mins, int secs);
 
-		/* Function to increment the time */
-		void add(Time& anotherTime);
-			/* adds seconds to seconds, minutes to minutes and
-			hours to hours, taking into account that
-			a day has 24 hours, an hour has 60 minutes
-			and a minute has 60 seconds */
 
-		/* Function to overload << operator */
-		friend std::ostream& operator << (std::ostream&, Time&);
+		/* Functions */
+
+		/* 
+		 * Increments the time by adding another time to this instance. Adds seconds to seconds, minutes to minutes and
+		   hours to hours, using modulo arithmatic to take into account that
+		   a day has 24 hours, an hour has 60 minutes
+		   and a minute has 60 seconds.
+		 *
+		 * @param anotherTime A reference to the Time object to be added.
+		 */
+		void add(Time& anotherTime);
+
+		/*
+		 * Overloads the << operator for Time.
+		 *
+		 * @param outstream A reference to the output stream.
+		 * @param time A reference to the Time object to display.
+		 * 
+		 * @return A reference to the output stream.
+		*/
+		friend std::ostream& operator << (std::ostream& outstream, Time& time);
 
 	private:
 
 		/* Attributes */
+
+		/* The three attributes store the hour, minute and second components of Time respectively.
+		 * They should only be adjusted through "add" method. */
 		int theHour;
 		int theMins;
 		int theSecs;
--- trafficLight.h
+++ trafficLight.h
@@ -1,7 +1,7 @@
 /* trafficLights.h - header file for the class TrafficLight */
 
 /* Author: Zev Menachemson 
- * Last Edited: 05/11/2024
+ * Last Edited: 18/11/2024
  */
 
 #ifndef TRAFFICLIGHT_H
@@ -17,17 +17,45 @@ class TrafficLight {
 	public:
 
 		/* Constructors */
-		TrafficLight(Time, std::string);
-		TrafficLight(Time, std::string, TrafficLight&);
 
-		/* Function for when car approaches crossing */
+		/* 
+		 * Parameterised constructor to be called for creating an instance
+		 * of the first traffic light in a given pair.
+		 *
+		 * @param delay1 The delay time associated with the first traffic light in the pair.
+		 * @param name1 The name of the first traffic light in the pair.
+		 */
+		TrafficLight(Time delay1, std::string name1);
+
+		/* 
+		 * Parameterised constructor to be called for creating an instance
+		 * of the second traffic light in a given pair. This constructor should only
+		 * be called after the first traffic light in the pair has already been instantiated,
+		 * otherwise an error message will be returned. This constructor will also set the pair
+		 * attribute of the corresponding traffic light to point to this traffic light.
+		 * 
+		 * @param delay2 The delay time associated with the second traffic light in the pair.
+		 * @param name2 The name of the second traffic light in the pair.
+		 * @param partner A reference to the partner of this traffic light.
+		 */
+		TrafficLight(Time delay2, std::string name2, TrafficLight& partner);
+
+		/* Functions */
+
+		/* 
+		 * Simulates the behaviour when a car signals to cross the crossing controlled
+		 * by this traffic light. This function initiates a series of behaviours and actions
+		 * based on the current light colour, its pair and their delay times
+		 */
 		void carWantsToCross();
 
-		/* Function to set the time */
-		static void setTheTime(Time&);
-
-		/* Functions to overload << operator */
-		friend std::ostream& operator << (std::ostream&, TrafficLight*);
+		/* 
+		 * Sets the time of the global clock. This is a static function
+		 * as all traffic lights must reference a shared global clock.
+		 *
+		 * @param time The instance of Time representing the global clock.
+		 */
+		static void setTheTime(Time& time);
 
 		enum Colours {red, yellow, green};
 		friend std::ostream &operator<<(std::ostream& out, Colours colour);
